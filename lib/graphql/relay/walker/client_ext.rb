5ac79b35dbe58dd9cdc750559a2c5ca58304bc4c
Brandon Black <brandonmblack@gmail.com>
2016-11-03 14:02:06 -0700

    adding ability to pass context and variables through to graphql client

--- lib/graphql/relay/walker/client_ext.rb
+++ lib/graphql/relay/walker/client_ext.rb
@@ -6,15 +6,21 @@ module GraphQL::Relay::Walker
     # &blk     - A block to call with each Walker::Frame that is visited.
     #
     # Returns nothing.
-    def walk(from_id:)
+    def walk(from_id:, variables: {}, context: {})
       query_string = GraphQL::Relay::Walker.query_string(schema)
       walker_query = parse(query_string)
 
       GraphQL::Relay::Walker.walk(from_id: from_id) do |frame|
-        response = query(walker_query, variables: {"id" => frame.gid})
+        response = query(
+          walker_query,
+          variables: variables.merge({"id" => frame.gid}),
+          context: context
+        )
+
         frame.context[:response] = response
-        frame.result = response.data.to_h
+        frame.result = (response.respond_to?(:data) && response.data) ? response.data.to_h : {}
         frame.enqueue_found_gids
+
         yield(frame) if block_given?
       end
     end
