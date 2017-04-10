1da76ca09c5693c718a6c212d1aca32cce77a59e
Brooks Swinnerton <bswinnerton@gmail.com>
2017-04-10 13:45:22 -0400

    Use valid_isolated_input?
    As per the deprecation notice noted in https://git.io/vS6hn.

--- lib/graphql/relay/walker/query_builder.rb
+++ lib/graphql/relay/walker/query_builder.rb
@@ -244,7 +244,12 @@ module GraphQL::Relay::Walker
       12.times.map { (SecureRandom.random_number(26) + 97).chr }.join
     end
 
-    if GraphQL::VERSION >= "1.4.0"
+    if GraphQL::VERSION >= "1.5.6"
+      def valid_input?(type, input)
+        allow_all = GraphQL::Schema::Warden.new(->(_) { false }, schema: schema, context: nil)
+        type.valid_isolated_input?(input, allow_all)
+      end
+    elsif GraphQL::VERSION >= "1.4.0"
       def valid_input?(type, input)
         allow_all = GraphQL::Schema::Warden.new(->(_) { false }, schema: schema, context: nil)
         type.valid_input?(input, allow_all)
